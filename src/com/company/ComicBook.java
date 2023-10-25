0df1987b2f94c97b50bad84d960e1cf5db140e1f
smgGG675 <sracimir@gmail.com>
2023-10-25 15:47:33 +0300

    Updated project

--- src/com/company/ComicBook.java
+++ src/com/company/ComicBook.java
@@ -1,5 +1,14 @@
 package com.company;
 
+import java.sql.SQLOutput;
+
 public class ComicBook extends Book{
     private String characterName;
+
+    @Override
+    public double getPrice() {
+        System.out.println(getTitle()+("is on sale 70%"));
+        double originalPrice=super.getPrice();
+        return originalPrice*07;
+    }
 }
